ba0e9d8141d0dc73291cbc2e3eb50a5f76461e92
etorresh <emiliotorreshinojosa@gmail.com>
2021-04-07 19:30:20 -0600

    About: add footer animation

--- src/app/sidebar-content/about/about.component.ts
+++ src/app/sidebar-content/about/about.component.ts
@@ -1,45 +1,27 @@
 import {Component} from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
-import {ThemeService} from '../theme.service';
+import {ParticlesService} from '../../particles.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
   animations: [
-    trigger('animateText', [
-      state('gray', style({
-        color: '#353e3d'
+    trigger('openClose', [
+      state('open', style({
+        left: 'calc(50% + 144px)'
       })),
-      state('blue', style({
-        color: '#2472a3'
+      state('close', style({
+        left: '50%'
       })),
-      state('orange', style({
-        color: '#a35024'
-      })),
-      state('yellow', style({
-        color: '#a39024'
-      })),
-      state('purple', style({
-        color: '#7d24a3'
-      })),
-      transition('gray => blue', [
-        animate('1s')
+      transition('open => close', [
+        animate('0.5s')
       ]),
-      transition('blue => orange', [
-        animate('1s')
-      ]),
-      transition('orange => yellow', [
-        animate('1s')
-      ]),
-      transition('yellow => purple', [
-        animate('1s')
-      ]),
-      transition('purple => blue', [
-        animate('1s')
+      transition('close => open', [
+        animate('0.25s')
       ]),
     ]),
   ]
 })
 export class AboutComponent {
-  constructor(public theme: ThemeService) { }
+  constructor(public particles: ParticlesService) { }
 }
