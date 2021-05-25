cfeb91de4199a60a0842c04dab3216176a1ac451
etorresh <emiliotorreshinojosa@gmail.com>
2021-05-24 18:42:02 -0600

    Default route redirects to different components on mobile and desktop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './sidebar-content/about/about.component';
 import {ExperienceComponent} from './sidebar-content/experience/experience.component';
 import {ProjectsComponent} from './sidebar-content/projects/projects.component';
@@ -7,7 +7,7 @@ import {SkillsComponent} from './sidebar-content/skills/skills.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: '', redirectTo: window.screen.width > 992 ? '/about' : '', pathMatch: 'full'},
   {path: 'about', component: AboutComponent},
   {path: 'experience', component: ExperienceComponent},
   {path: 'projects', component: ProjectsComponent},
