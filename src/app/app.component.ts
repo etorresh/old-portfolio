dc015600ddc09170344351b4bf7c85ff9f774735
etorresh <emiliotorreshinojosa@gmail.com>
2021-06-06 17:25:09 -0600

    added secret console message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -79,6 +79,7 @@ export class AppComponent implements OnInit, OnDestroy{
         this.sidebarService.setActive(false);
       }
     });
+    console.log('%c Hello! \n Thank you for visiting my portfolio.\n There\'s nothing else to see in the console, but I\'m glad you checked.\n Oh! Before you go, I also want to let you know that I\'m looking for new opportunities. Feel free to reach out! \n Have a great day!', 'background: #222; color: #bada55');
   }
 
   @HostListener('window:resize', ['$event'])
